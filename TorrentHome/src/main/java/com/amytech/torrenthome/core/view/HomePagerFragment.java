179d2cfa6b3ee1e2477150d4fdba9c8f5408279e
amytech <448601869@qq.com>
2015-08-13 18:30:31 +0800

    remove root path.

--- TorrentHome/src/main/java/com/amytech/torrenthome/core/view/HomePagerFragment.java
+++
@@ -1,9 +0,0 @@
-package com.amytech.torrenthome.core.view;
-
-import android.support.v4.app.Fragment;
-
-/**
- * Created by zhaitianlong on 15/8/12.
- */
-public class HomePagerFragment extends Fragment {
-}
